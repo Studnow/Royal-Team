53cc658207f8778969c1971526581cf0fd5c262b
Igor <silent659@gmail.com>
2026-01-22 23:55:41 +0200

    testing api script

--- assets/extractedText.js
+++ assets/extractedText.js
@@ -1,207 +1,368 @@
 export default {
-  "text": {
-    "desktopmainpage": [
-      "→ 🇪🇸 Spain",
-      "→ 🇵🇹 Portugal",
-      "+34 674 585 083",
-      "+34 674 585 083\n+34 617 507 344",
-      "+8% tourists spend more than €100bn a year including rentals",
-      "$900/per m",
-      "08/08/25",
-      "10+ years of experience",
-      "100 m2",
-      "200 m2",
-      "350 days ourist season throughout most of the year in coastal regions",
-      "80% average occupancy rate in 2024 (based on Spanish tourist rental data)",
-      "About",
-      "About us",
-      "After-Sales\nService\nOngoing support\nand\nconsultation\nafter deal\ncompletion",
-      "Agents",
-      "All Properties",
-      "apartment name",
-      "Auto",
-      "Bosnia",
-      "Buy Commercial Property",
-      "Buy Flat",
-      "Buy Hotel",
-      "Buy House",
-      "Canada",
-      "Cars for Sale",
-      "Comfortable Complexes",
-      "Comfortable furniture, unique interior solutions, and attention to detail for a cozy, short-term stay, tailored to tourist needs in Spain.",
-      "contact",
-      "Contact",
-      "CONTACT NOW FOR FREE CONSULTATION",
-      "Countries of our reach",
-      "Create a source of constant passive income through investment in low-rise properties in Spain",
-      "Email",
-      "Enjoy breathtaking Mediterranean sea views from the terraces, ideal for relaxation and attracting investors.",
-      "FAQ",
-      "Featured Investment",
-      "Featured Investment Opportunities",
-      "Free Consultation",
-      "From search to closing the deal",
-      "Full transaction support",
-      "G-MSK INVEST",
-      "Gana",
-      "Germany",
-      "Home",
-      "How we work",
-      "in the Spanish market",
-      "info@gmskinvest.com",
-      "Information",
-      "Iraq",
-      "it’s professional help \nfinding and buying property in Spain",
-      "Legal Support\nComplete legal\nsupport and\ndocumentation\nprocessing",
-      "Location",
-      "lorem",
-      "Lorem ipsum dolor sit amet, consectet",
-      "Message",
-      "Modern Equipment",
-      "Morocco",
-      "Name",
-      "News",
-      "Newsletter",
-      "No risks, no hidden fees",
-      "Original Design Solutions",
-      "Other Services",
-      "Our real estate agency has a range of luxury and exclusive properties that are perfect for international clients.",
-      "our services",
-      "Panoramic Views",
-      "Phone Number",
-      "Poland",
-      "Portugal",
-      "Project Development and Management",
-      "Properties",
-      "Property Management",
-      "Property Management\nProfessional management\nand income maximization",
-      "Property Selection\nMarket analysis and\nselection of properties with\nhigh yield potential",
-      "Providing modern technologies for comfort, safety, and high quality and uniqueness of the property, tailored to Mediterranean living.",
-      "Quality of Construction",
-      "Ranks in the top 3 places for tourism and relaxation in Spain",
-      "Read",
-      "Real estate",
-      "Real Estate",
-      "Rent Commercial Property",
-      "Rent Flat",
-      "Rent Hotel",
-      "Rent House",
-      "Rental Cars",
-      "Send",
-      "Singapur",
-      "Sinigal",
-      "Spacious areas, stunning sea views, and a high level of service, ensuring maximum comfort and personal space for tourists.",
-      "Spain",
-      "State-of-the-art technology for enhanced comfort and convenience in Spanish properties.",
-      "UAE",
-      "United Kingdom",
-      "USA",
-      "Verified properties only",
-      "Visa & Residency",
-      "Visa/Residency",
-      "We ensure constant property maintenance with our team, providing full service and maximum returns for large investments in Spain.",
-      "We speak your language",
-      "Why Choose Us?",
-      "Why invest in Spain & Portugal?",
-      "Work with international clients",
-      "Your Trusted Investment Partner"
-    ],
-    "mobilemain": [
-      "→ 🇪🇸 Spain",
-      "→ 🇵🇹 Portugal",
-      "+34 617 507 344",
-      "+34 674 585 083",
-      "$900/per m",
-      "08/08/25",
-      "10+ years of experience",
-      "100 m2",
-      "200 m2",
-      "About",
-      "All news",
-      "All Properties",
-      "apartment name",
-      "Article name",
-      "Auto",
-      "Bosnia",
-      "Buy Commercial Property",
-      "Buy Flat",
-      "Buy Hotel",
-      "Buy House",
-      "Canada",
-      "Cars for Sale",
-      "Comfortable Complexes",
-      "Comfortable furniture, unique interior solutions, and attention to detail for a cozy, short-term stay, tailored to tourist needs in Spain",
-      "contact",
-      "Contact",
-      "CONTACT NOW FOR FREE CONSULTATION",
-      "Countries of our reach",
-      "Create a source of constant passive income through investment in low-rise properties in Spains",
-      "Email",
-      "Enjoy breathtaking Mediterranean sea views from the terraces, ideal for relaxation and attracting investors",
-      "Featured Investment",
-      "Featured Investment Opportunities",
-      "Free Consultation",
-      "From search to closing the deal",
-      "Full transaction support",
-      "G-MSK INVEST",
-      "Gana",
-      "Germany",
-      "Home",
-      "in the Spanish market",
-      "info@gmskinvest.com",
-      "Information",
-      "Iraq",
-      "Location",
-      "lorem",
-      "Lorem ipsum dolor sit amet, consectet",
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
-      "Mergers & Acquisition",
-      "Message",
-      "Modern Equipment",
-      "Morocco",
-      "Name",
-      "News",
-      "Newsletter",
-      "No risks, no hidden fees",
-      "Original Design Solutions",
-      "Other Services",
-      "Our real estate agency has a range of luxury and exclusive properties that are perfect for international clients.",
-      "our services",
-      "Panoramic Views",
-      "Phone Number",
-      "Poland",
-      "Portugal",
-      "Project Development and Management",
-      "Properties",
-      "Property Management",
-      "Providing modern technologies for comfort, safety, and high quality and uniqueness of the property, tailored to Mediterranean living.",
-      "Quality of Construction",
-      "Ranks in the top 3 places for tourism and relaxation in Spain",
-      "Read",
-      "Real estate",
-      "Real Estate",
-      "Rent Commercial Property",
-      "Rent Flat",
-      "Rent Hotel",
-      "Rent House",
-      "Rental Cars",
-      "Send",
-      "Singapur",
-      "Sinigal",
-      "Spacious areas, stunning sea views, and a high level of service, ensuring maximum comfort and personal space for tourists.",
-      "Spain",
-      "State-of-the-art technology for enhanced comfort and convenience in Spanish properties.",
-      "UAE",
-      "United Kingdom",
-      "USA",
-      "Verified properties only",
-      "Visa & Residency",
-      "Visa/Residency",
-      "We ensure constant property maintenance with our team, providing full service and maximum returns for large investments in Spain.",
-      "We speak your language",
-      "Why Choose Us?",
-      "Work with international clients",
-      "Your Trusted Investment Partner"
-    ]
+  "Design": {
+    "hero": {
+      "text": [
+        "Ranks in the top 3 places for tourism and relaxation in Spain",
+        "80% average occupancy rate in 2024 (based on Spanish tourist rental data)",
+        "+8% tourists spend more than €100bn a year including rentals",
+        "350 days ourist season throughout most of the year in coastal regions",
+        "Your Trusted Investment Partner",
+        "G-MSK INVEST"
+      ],
+      "images": [
+        {
+          "name": "example_image",
+          "ext": "png"
+        }
+      ]
+    },
+    "btn": {
+      "text": [
+        "Free Consultation",
+        "Read",
+        "All Properties",
+        "Send",
+        "+34 674 585 083",
+        "All news"
+      ],
+      "images": [
+        {
+          "name": "example_image",
+          "ext": "png"
+        }
+      ]
+    },
+    "headline": {
+      "text": [
+        "our services",
+        "Real estate",
+        "CONTACT NOW FOR FREE CONSULTATION",
+        "Create a source of constant passive income through investment in low-rise properties in Spains",
+        "Article name",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation"
+      ],
+      "images": [
+        {
+          "name": "example_image",
+          "ext": "png"
+        }
+      ]
+    },
+    "servicecard": {
+      "text": [
+        "Real estate",
+        "Project Development and Management",
+        "Auto",
+        "Visa/Residency"
+      ],
+      "images": [
+        {
+          "name": "example_image",
+          "ext": "png"
+        }
+      ]
+    },
+    "set": {
+      "text": [
+        "08/08/25"
+      ],
+      "images": [
+        {
+          "name": "example_image",
+          "ext": "png"
+        }
+      ]
+    },
+    "section5": {
+      "text": [
+        "G-MSK INVEST",
+        "it’s professional help finding and buying property in Spain",
+        "Property Selection Market analysis and selection of properties with high yield potential",
+        "Property Management Professional management and income maximization",
+        "Legal Support Complete legal support and documentation processing",
+        "After-Sales Service Ongoing support and consultation after deal completion",
+        "Create a source of constant passive income through investment in low-rise properties in Spain",
+        "Quality of Construction",
+        "Providing modern technologies for comfort, safety, and high quality and uniqueness of the property, tailored to Mediterranean living.",
+        "Comfortable Complexes",
+        "Spacious areas, stunning sea views, and a high level of service, ensuring maximum comfort and personal space for tourists.",
+        "Modern Equipment",
+        "State-of-the-art technology for enhanced comfort and convenience in Spanish properties.",
+        "Property Management",
+        "We ensure constant property maintenance with our team, providing full service and maximum returns for large investments in Spain.",
+        "Panoramic Views",
+        "Enjoy breathtaking Mediterranean sea views from the terraces, ideal for relaxation and attracting investors.",
+        "Original Design Solutions",
+        "Comfortable furniture, unique interior solutions, and attention to detail for a cozy, short-term stay, tailored to tourist needs in Spain."
+      ],
+      "images": [
+        {
+          "name": "example_image",
+          "ext": "png"
+        }
+      ]
+    },
+    "section3": {
+      "text": [
+        "Why Choose Us?",
+        "in the Spanish market",
+        "10+ years of experience",
+        "No risks, no hidden fees",
+        "Verified properties only",
+        "From search to closing the deal",
+        "Full transaction support",
+        "Work with international clients",
+        "We speak your language"
+      ],
+      "images": [
+        {
+          "name": "example_image",
+          "ext": "png"
+        }
+      ]
+    },
+    "pic": {
+      "text": [
+        "lorem"
+      ],
+      "images": [
+        {
+          "name": "example_image",
+          "ext": "png"
+        }
+      ]
+    },
+    "list": {
+      "text": [
+        "apartment name",
+        "Location",
+        "Lorem ipsum dolor sit amet, consectet"
+      ],
+      "images": [
+        {
+          "name": "example_image",
+          "ext": "png"
+        }
+      ]
+    },
+    "item": {
+      "text": [
+        "$900/per m",
+        "200 m2",
+        "100 m2"
+      ],
+      "images": [
+        {
+          "name": "example_image",
+          "ext": "png"
+        }
+      ]
+    },
+    "section": {
+      "text": [
+        "Our real estate agency has a range of luxury and exclusive properties that are perfect for international clients.",
+        "Information",
+        "Name",
+        "Phone Number",
+        "Countries of our reach",
+        "Email",
+        "Message"
+      ],
+      "images": [
+        {
+          "name": "example_image",
+          "ext": "png"
+        }
+      ]
+    },
+    "country": {
+      "text": [
+        "Spain",
+        "United Kingdom",
+        "Poland",
+        "Bosnia",
+        "Iraq",
+        "UAE",
+        "Singapur",
+        "Germany",
+        "Portugal",
+        "Morocco",
+        "Sinigal",
+        "Gana",
+        "USA",
+        "Canada"
+      ],
+      "images": [
+        {
+          "name": "example_image",
+          "ext": "png"
+        }
+      ]
+    },
+    "menuitem": {
+      "text": [
+        "Real Estate",
+        "Rent Flat",
+        "Rent House",
+        "Buy Flat",
+        "Buy House",
+        "Project Development and Management",
+        "Properties",
+        "Featured Investment Opportunities",
+        "Other Services",
+        "Rental Cars",
+        "Cars for Sale",
+        "Visa & Residency",
+        "→ 🇪🇸 Spain",
+        "→ 🇵🇹 Portugal",
+        "About",
+        "Why invest in Spain & Portugal?",
+        "About us",
+        "Agents",
+        "How we work",
+        "FAQ",
+        "News",
+        "Home",
+        "Featured Investment",
+        "Contact",
+        "Mergers & Acquisition"
+      ],
+      "images": [
+        {
+          "name": "example_image",
+          "ext": "png"
+        }
+      ]
+    },
+    "dropitem": {
+      "text": [
+        "Rent Hotel",
+        "Rent Commercial Property",
+        "Buy Hotel",
+        "Buy Commercial Property"
+      ],
+      "images": [
+        {
+          "name": "example_image",
+          "ext": "png"
+        }
+      ]
+    },
+    "footer": {
+      "text": [
+        "Newsletter",
+        "Phone Number",
+        "contact",
+        "+34 674 585 083 +34 617 507 344",
+        "info@gmskinvest.com",
+        "+34 674 585 083",
+        "+34 617 507 344"
+      ],
+      "images": [
+        {
+          "name": "example_image",
+          "ext": "png"
+        }
+      ]
+    },
+    "mobilehero": {
+      "text": [
+        "G-MSK INVEST",
+        "Your Trusted Investment Partner",
+        "Ranks in the top 3 places for tourism and relaxation in Spain"
+      ],
+      "images": [
+        {
+          "name": "example_image",
+          "ext": "png"
+        }
+      ]
+    },
+    "mobsection3": {
+      "text": [
+        "Why Choose Us?",
+        "10+ years of experience",
+        "in the Spanish market",
+        "Verified properties only",
+        "No risks, no hidden fees",
+        "Full transaction support",
+        "From search to closing the deal",
+        "We speak your language",
+        "Work with international clients"
+      ],
+      "images": [
+        {
+          "name": "example_image",
+          "ext": "png"
+        }
+      ]
+    },
+    "mobsection4": {
+      "text": [
+        "Quality of Construction",
+        "Providing modern technologies for comfort, safety, and high quality and uniqueness of the property, tailored to Mediterranean living.",
+        "Comfortable Complexes",
+        "Spacious areas, stunning sea views, and a high level of service, ensuring maximum comfort and personal space for tourists.",
+        "Original Design Solutions",
+        "Comfortable furniture, unique interior solutions, and attention to detail for a cozy, short-term stay, tailored to tourist needs in Spain",
+        "Panoramic Views",
+        "Enjoy breathtaking Mediterranean sea views from the terraces, ideal for relaxation and attracting investors",
+        "Modern Equipment",
+        "State-of-the-art technology for enhanced comfort and convenience in Spanish properties.",
+        "Property Management",
+        "We ensure constant property maintenance with our team, providing full service and maximum returns for large investments in Spain."
+      ],
+      "images": [
+        {
+          "name": "example_image",
+          "ext": "png"
+        }
+      ]
+    },
+    "mobsection5": {
+      "text": [
+        "Real estate",
+        "apartment name",
+        "Location",
+        "Lorem ipsum dolor sit amet, consectet",
+        "News"
+      ],
+      "images": [
+        {
+          "name": "example_image",
+          "ext": "png"
+        }
+      ]
+    },
+    "mobsection6": {
+      "text": [
+        "CONTACT NOW FOR FREE CONSULTATION",
+        "Our real estate agency has a range of luxury and exclusive properties that are perfect for international clients.",
+        "Information",
+        "Name",
+        "Phone Number",
+        "Email",
+        "Message"
+      ],
+      "images": [
+        {
+          "name": "example_image",
+          "ext": "png"
+        }
+      ]
+    },
+    "no-section": {
+      "text": [
+        "80% average occupancy rate in 2024 (based on Spanish tourist rental data)",
+        "+8% tourists spend more than €100bn a year including rentals",
+        "350 days ourist season throughout most of the year in coastal regions"
+      ],
+      "images": [
+        {
+          "name": "example_image",
+          "ext": "png"
+        }
+      ]
+    }
   }
 };
